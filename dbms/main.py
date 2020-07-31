7522a3cabb19c511ec300a219ef1ef6b310f7b03
JanBat <janbatelka@gmail.com>
2020-07-31 12:33:16 +0200

    parser steps 0+2

--- dbms/main.py
+++ dbms/main.py
@@ -1,4 +1,7 @@
+import re
+import functools
 from typing import List, Union
+
 
 test_database = "test_db"
 sql_teststring: str = "SELECT firstname\n" \
@@ -77,12 +80,42 @@ class Database:
         # return table created by final operation
         raise NotImplementedError
 
+testquery =\
+"SELECT fav_food\n" \
+"FROM TEST T\n" \
+"WHERE T.fistname == bob\n" \
+"AND T.lastname == burnquist\n"
+
 
 class Parser:
     KEYWORDS = ["SELECT", "FROM", "WHERE"]  # "GROUP BY"
     @staticmethod
     def parse(input: str) -> Query:
         # create Query-tree from sql-string
+
+        # STEP 0: preliminary string cleanup
+        clean = input.replace('\n', ' ')
+
+        # STEP 1: extract nested queries, treat recursively (implement later, nice-to-have for now)
+        outer = clean  # (implement later if needed
+
+        # STEP 2: identify keywords, separate remaining string into (keyword, parameter) dictionaries
+        keyword_parameter_dicts = []
+        words = outer.split(' ')
+        for word in words:
+            if word in Parser.KEYWORDS:
+                keyword_parameter_dicts.append({'keyword': word, 'parameter': []})
+            else:
+                keyword_parameter_dicts[-1]['parameter'].append(word)
+
+        for d in keyword_parameter_dicts:
+            d['parameter'] = functools.reduce(lambda x, y: x + y + ' ', d['parameter'], '')[:-1]
+
+        # STEP 3: identify & substitute(homogenize) aliases
+        print(keyword_parameter_dicts)
+        # STEP 4: create Query.Node for each (keyword, parameter) tuple and connect the nodes
+
+
         raise NotImplementedError
 
 
@@ -91,6 +124,10 @@ if __name__ == "__main__":
     # database = Database(test_database)
     # query = Parser.parse(sql_teststring)
     # database.query(query)
-    test = project(load('test_db', 'TEST'), ['fav_food'])
 
-    print(test)
+    Parser.parse(testquery)
+
+    #test = project(load('test_db', 'TEST'), ['fav_food'])
+    #print (testquery)
+
+    #print(test)
